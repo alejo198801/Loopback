f05c1604b326ca716002a5ff392b3d1e9e2afa14
Alejandro Castaño <alcastano001@gmail.com>
2021-11-11 16:49:43 -0500

    creando la aoo de loopback

---
+++ backend_loopback/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {AppLookback} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new AppLookback({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: AppLookback;
+  client: Client;
+}
